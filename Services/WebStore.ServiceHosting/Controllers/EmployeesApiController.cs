74887ba66e8a0299d854404dcf9df3adb1068b8c
German322VDK <germean322@gmail.com>
2021-04-19 16:13:56 +1000

    WebApi + Employee + устронения косяка json файла

--- Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -2,11 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using WebStore.Domain.Models;
+using WebStore.Interfaces;
 using WebStore.Interfaces.Services;
 
 namespace WebStore.ServiceHosting.Controllers
 {
-    [Route("api/[employees]")]
+    [Route(WebAPI.Employees)]
     [ApiController]
     public class EmployeesApiController : ControllerBase, IEmployeesData
     {
