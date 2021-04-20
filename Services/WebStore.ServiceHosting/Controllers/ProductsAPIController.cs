f4f36a88252e3da87a9aaf84f11a4865c4e75552
German322VDK <germean322@gmail.com>
2021-04-20 10:44:16 +1000

    Контроллер каталога товаров WebAPI

---
+++ Services/WebStore.ServiceHosting/Controllers/ProductsAPIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Domain;
+using WebStore.Domain.DTO;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Products)]
+    [ApiController]
+    public class ProductsAPIController : ControllerBase, IProductData
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsAPIController(IProductData ProductData) => _ProductData = ProductData;
+
+        [HttpGet("brands")]
+        public IEnumerable<BrandDTO> GetBrands() => _ProductData.GetBrands();
+
+        [HttpGet("{id:int}")]
+        public ProductDTO GetProductById(int id) => _ProductData.GetProductById(id);
+
+        [HttpPost]
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null) =>
+            _ProductData.GetProducts(Filter);
+
+        [HttpGet("sections")]
+        public IEnumerable<SectionDTO> GetSections() => _ProductData.GetSections();
+    }
+}
