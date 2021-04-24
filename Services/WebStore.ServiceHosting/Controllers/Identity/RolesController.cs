85e548849001bac556264195075ec6563e3a56a9
German322VDK <germean322@gmail.com>
2021-04-24 19:35:10 +1000

    Заготовки контроллеров User и Role

---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/RolesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+using WebStrore.DAL.Context;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.Roles)]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<Role> _RoleStore;
+
+        public RolesController(WebStoreDB db) => _RoleStore = new RoleStore<Role>(db);
+    }
+}
