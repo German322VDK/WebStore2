4a820cc1a7269a9d2bd611efc80c3f0590601942
гера шлычков <germean322@gmail.com>
2021-04-30 12:32:32 +1000

    Инфраструктура логирования

---
+++ Common/WebStore.Logger/Log4NETLoggerFactoryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace WebStore.Logger
+{
+    public static class Log4NETLoggerFactoryExtensions
+    {
+        private static string CheckFilePath(string FilePath)
+        {
+            if (FilePath is not { Length: > 0 }) 
+                throw new ArgumentException("Некорректный путь к файлу", nameof(FilePath));
+
+            if (Path.IsPathRooted(FilePath)) return FilePath;
+
+            var assembly = Assembly.GetEntryAssembly();
+            var dir = Path.GetDirectoryName(assembly!.Location);
+
+            return Path.Combine(dir!, FilePath);
+        }
+
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory, 
+            string ConfiguratioFile = "log4net.config")
+        {
+            Factory.AddProvider(new Log4NETLoggerProvider(CheckFilePath(ConfiguratioFile)));
+
+            return Factory;
+        }
+    }
+}
---
+++ Common/WebStore.Logger/Log4NETLoggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Xml;
+using System.Collections.Concurrent;
+
+namespace WebStore.Logger
+{
+    public class Log4NETLoggerProvider : ILoggerProvider
+    {
+        private readonly string _ConfiguratioFile;
+        private readonly ConcurrentDictionary<string, Log4NETLogger> _Loggers = new();
+
+       public Log4NETLoggerProvider(string ConfiguratioFile) => _ConfiguratioFile = ConfiguratioFile;
+
+        public ILogger CreateLogger(string Category) =>
+            _Loggers.GetOrAdd(Category, category =>
+            {
+                var xml = new XmlDocument();
+                xml.Load(_ConfiguratioFile);
+                return new Log4NETLogger(category, xml["log4net"]);
+            });
+
+        public void Dispose() => _Loggers.Clear();
+    }
+}
