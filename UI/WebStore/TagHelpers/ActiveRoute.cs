9963973c6b43e3da7b25c37f3d465b7ec434c90e
гера шлычков <germean322@gmail.com>
2021-05-06 20:12:17 +1000

    tag-helper игнорирования

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -11,6 +11,7 @@ namespace WebStore.TagHelpers
     public class ActiveRoute : TagHelper
     {
         private const string AttributeName = "is-active-route";
+        private const string IgnoreAction = "ignore-action";
 
         [HtmlAttributeName("asp-controller")]
         public string Controller { get; set; }
@@ -27,13 +28,16 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (IsActive())
+            var ignore_action = output.Attributes.ContainsName(IgnoreAction);
+
+            if (IsActive(ignore_action))
                 MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
+            output.Attributes.RemoveAll(IgnoreAction);
         }
 
-        private bool IsActive()
+        private bool IsActive(bool IsIgnoreAction)
         {
             var route_values = ViewContext.RouteData.Values;
             var current_controller = route_values["controller"]?.ToString();
@@ -44,7 +48,7 @@ namespace WebStore.TagHelpers
             if (!string.IsNullOrEmpty(Controller) && !string.Equals(current_controller, Controller, str))
                 return false;
 
-            if (!string.IsNullOrEmpty(Action) && !string.Equals(current_action, Action, str))
+            if (!IsIgnoreAction && !string.IsNullOrEmpty(Action) && !string.Equals(current_action, Action, str))
                 return false;
 
             foreach (var (key, value) in RouteValues)
