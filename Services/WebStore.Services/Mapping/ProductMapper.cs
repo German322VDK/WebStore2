1bcb1a1396dc4f2b28c1ed8f0b9394c431a7ed0f
гера шлычков <germean322@gmail.com>
2021-05-13 17:48:58 +1000

    Подгатовка разметки страниц каталога товаров

--- Services/WebStore.Services/Mapping/ProductMapper.cs
+++ Services/WebStore.Services/Mapping/ProductMapper.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using WebStore.Domain.DTO;
 using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels;
--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -5,11 +5,13 @@ using WebStore.Interfaces.Services;
 using WebStore.Domain.ViewModels;
 using WebStore.Services.Mapping;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
 namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
+        private const string __CatalogPageSize = "CatalogPageSize";
         private readonly IProductData _ProductData;
         private readonly IConfiguration _Configuration;
 
@@ -22,7 +24,7 @@ namespace WebStore.Controllers
         public IActionResult Index(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
         {
             var page_size = PageSize ??
-                (int.TryParse(_Configuration["CatalogPageSize"], out var value) ? value : null);
+                (int.TryParse(_Configuration[__CatalogPageSize], out var value) ? value : null);
 
             var filter = new ProductFilter
             {
@@ -55,5 +57,24 @@ namespace WebStore.Controllers
 
             return View(product.FromDTO().ToView());
         }
+
+        #region WebAPI
+
+        public IActionResult GetFeaturesItems(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null) =>
+            PartialView("Partial/_FeaturesItems", GetProducts(BrandId, SectionId, Page, PageSize));
+
+        private IEnumerable<ProductViewModel> GetProducts(int? BrandId, int? SectionId, int Page, int? PageSize) =>
+            _ProductData.GetProducts(new ProductFilter
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Page = Page,
+                PageSize = PageSize ?? (int.TryParse(_Configuration[__CatalogPageSize], out var size) ? size : null)
+            })
+               .Products.OrderBy(p => p.Order)
+               .FromDTO()
+               .ToView();
+
+        #endregion
     }
 }
